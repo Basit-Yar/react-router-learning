1915a3915e7ed26224b361ad053c7486333da745
SyedBasit <basityar6@gmail.com>
2024-05-02 17:29:08 +0530

    get familiar with useLoaderData hook, to optimize the fetching data

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 
 function Github() {
 
-    let username = "Basit-Yar";
-    let [data, setData] = useState([]);
+    let data = useLoaderData();
+    // let username = "Basit-Yar";
+    // let [data, setData] = useState([]);
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        fetch(`https://api.github.com/users/${username}`)
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                console.log(data);
-                setData(data);
-            })
+    //     fetch(`https://api.github.com/users/${username}`)
+    //         .then((res) => {
+    //             return res.json();
+    //         })
+    //         .then((data) => {
+    //             console.log(data);
+    //             setData(data);
+    //         })
 
-    }, [])
+    // }, [])
 
     return (
 
         <div className="flex justify-around py-16">
             <img className="w-1/4" src={data.avatar_url} alt="img" />
             <div className="flex flex-col justify-evenly font-semibold">
-                <h2>UserName : <span className="text-orange-700">{username}</span></h2>
+                <h2>UserName : <span className="text-orange-700">{data.login}</span></h2>
                 <h2>User Full Name : <span className="text-orange-700">{data.name}</span></h2>
                 <h2>Followers : <span className="text-orange-700">{data.followers}</span></h2>
                 <h2>Following : <span className="text-orange-700">{data.following}</span></h2>
@@ -34,3 +36,9 @@ function Github() {
 }
 
 export default Github;
+
+let username = "Basit-Yar";
+export const githubInfoLoader = async () => {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    return res.json();
+}
